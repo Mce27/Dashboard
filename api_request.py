5f818195aa5bb79d9cec1224781dbee85ab6e805
Matt Emerick <matthewemerick1@gmail.com>
2022-08-29 21:38:47 -0400

    Intitial commit

---
+++ api_request.py
@@ -0,0 +1,26 @@
+from tokenize import String
+import requests
+import json
+
+def test_api_pull(link:str):
+    response = requests.get(link)
+    print("\nStatus code = " +str(response.status_code))
+    print("\n\nContent = "+ str(response.content))
+    print("\n\nText = "+ str(response.text))
+    print("\n\nJson = "+ str(response.json()))
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+    
+
+def main():
+    print("Api output:\n")
+    response = requests.get("https://api.weather.gov/gridpoints/BUF/74,60/forecast/hourly")
+    json = response.json()
+    properties = json["properties"]["periods"]
+
+    jprint(properties)
+
+main()
